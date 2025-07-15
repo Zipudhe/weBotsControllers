9f00572600ee4669582f62e5b8102db2efe91b62
Edyo <edyoandrade@gmail.com>
2025-07-15 13:30:40 -0300

    refactor(refaz o DinoRegressor de forma mais organizada): a

---
+++ RobotController/model/DinoRegressor.py
@@ -0,0 +1,73 @@
+from matplotlib import pyplot as plt
+import torch
+from torch.utils.data import Dataset
+import torch.nn as nn
+import numpy as np
+from transformers import AutoModel, AutoImageProcessor
+
+
+class DinoRegressor(nn.Module): 
+    def __init__(self, dino_hidden_size=768, top_k=4):
+        super(DinoRegressor, self).__init__()
+        self.dino_hidden_size = dino_hidden_size
+        self.top_k = top_k
+        
+        self.imgProcessor = AutoImageProcessor.from_pretrained("facebook/dinov2-base", use_fast=True)
+        self.dino = AutoModel.from_pretrained("facebook/dinov2-base", device_map="auto", output_attentions=True, attn_implementation="eager")
+        
+        self.regressor = nn.Sequential( # 1 camada oculta
+            nn.Linear(self.dino_hidden_size*(self.top_k + 1)*2, 4096),
+            nn.BatchNorm1d(4096),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(4096, 2048),
+            nn.BatchNorm1d(2048),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(2048, 2) # dist e angle
+        )
+        
+        # freeze dino
+        for params in self.dino.parameters():
+            params.require_grad = False
+        
+    def extract_features(self, images, top_k=4):
+        features = []
+        for image in images:
+            inputs = self.imgProcessor(images=image, return_tensors="pt", device='cuda' if torch.cuda.is_available() else 'cpu')
+            outputs = self.dino(**inputs)
+            
+            embeddings = outputs.last_hidden_state[0, :, :].detach().numpy()
+            cls_token = embeddings[0, :]
+                
+            # pega os patches com maiores attention
+            attentions = outputs.attentions[-1].detach().numpy()
+            attention = attentions[0, :, 0, 1:].mean(axis=0)
+            top_patches = np.argsort(attention)[-top_k:]
+            top_patch_emb = embeddings[1:][top_patches].flatten()
+                
+            features.append(np.concatenate([cls_token, top_patch_emb]))
+        
+        return np.concatenate(features)
+
+    def forward(self, x):
+        features = self.extract_features(x, top_k=self.top_k)
+        
+        return self.regressor(features)
+    
+class RegressionDataset(Dataset):
+    def __init__(self):
+        super(RegressionDataset, self).__init__()
+        data = np.load('RobotController/training_data.npz')
+        
+        self.rgb_data = torch.from_numpy(data['rgb'])
+        self.depth_data = torch.from_numpy(data['depth'])
+        self.dist_data = torch.from_numpy(data['dist'])
+        self.angle_data = torch.from_numpy(data['angle'])
+
+
+    def __getitem__(self, index):
+        return self.rgb_data[index], self.depth_data[index], self.dist_data[index], self.angle_data[index]
+
+    def __len__(self):
+        return len(self.rgb_data)
